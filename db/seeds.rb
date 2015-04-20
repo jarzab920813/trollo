6c69e377ac4eca71a9e3c237b84bc6330c74fc78
jarzab920813 <jarzab920813@wp.pl>
2015-04-20 15:14:19 +0200

    dodawanie admina

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,13 @@
 
 # INSERT INTO `users` (`id`, `name`, `surname`, `email`, `encrypted_password`, `reset_password_token`, `reset_password_sent_at`, `remember_created_at`, `sign_in_count`, `current_sign_in_at`, `last_sign_in_at`, `current_sign_in_ip`, `last_sign_in_ip`, `created_at`, `updated_at`, `admin`) VALUES
 # (1, 'QWER', 'Asdf', 'qwer@qwer.com', '$2a$10$hD9hQeGlX1vBMHwWp0lnoeK4Q/tZ5OU2m3T0.q3yEa8zX1s4tYYIi', NULL, NULL, NULL, 4, '2015-03-30 18:50:32', '2015-03-30 18:26:46', '127.0.0.1', '127.0.0.1', '2015-03-30 16:34:06', '2015-03-30 18:55:21', 1)
+unless User.any?
+	puts "Dodaje Admina"
+  User.create!(
+  name: 'QWER',
+  surname: 'Asdf',
+  email: 'qwer@qwer.com',
+  password: 'Qwer1234',
+  password_confirmation: 'Qwer1234',
+  admin: '1')
+end
